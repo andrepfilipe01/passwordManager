cbbe4c98e167bcfb98fa19287ef2626c2cb5f4d3
andrepfilipe <73351738+andrepfilipe01@users.noreply.github.com>
2025-03-17 22:32:30 +0000

    Update DatabaseConnection.java

--- PasswordManager/src/DatabaseConnection.java
+++ PasswordManager/src/DatabaseConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/password_manager";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "andre";
+    private static final String URL = "jdbc:postgresql://localhost:XXX/name_database";
+    private static final String USER = "xxxx"; // your user
+    private static final String PASSWORD = "xxxx"; // your pass
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
